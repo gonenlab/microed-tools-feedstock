979f36c91d06525257c326fa22af9737df386704
Johan Hattne <hattne@users.noreply.github.com>
2022-05-10 06:15:44 -0700

    Add MicroED tools 0.1.0-dev.5 (#1)
    * Add MicroED tools 0.1.0-dev.5
    
    * MNT: Re-rendered with conda-build 3.21.8, conda-smithy 3.19.0, and conda-forge-pinning 2022.05.05.18.43.04
    
    * Attempt to fix build and lint
    * Put numpy and python in host and run, but ignore the corresponding
      exports, as they are presumably dlopen(3):ed
    * Use pkg-config on Unix; macOS build breaks without it
    * Unbreak directory layout on Windows
    
    * Attempt to fix build
    * Force CMake to use sysroot's iconv on Linux and macOS
    * Need vc 14 to build with Python 3.5-3.10 on Windows
    
    * Attempt to fix build
    * Use matching Visual Studio generator for CMake
    * Different python form in run requirements
    
    * Attempt to fix build
    * Use conda_build_config.yaml to ask for vs2015 in addition to
      m2w64-toolchain on Windows
    
    * Attempt to fix build
    * Use recommended NMake Makefiles generator with CMake for Windows
      Python module
    
    * Attempt to fix build
    * Skip everything but Python module on Windows, build with default
      compiler
    
    * MNT: Re-rendered with conda-build 3.21.8, conda-smithy 3.19.0, and conda-forge-pinning 2022.05.09.07.08.30
    
    * Attempt to fix build
    * Explicitely set the build type
    
    * Attempt to fix build
    * Restore Windows compiler configuration
    
    * MNT: Re-rendered with conda-build 3.21.8, conda-smithy 3.19.0, and conda-forge-pinning 2022.05.09.07.08.30
    
    * Attempt to fix build
    * Revert Windows compiler configuration, now with explicit dependency on
      m2w64-toolchain
    
    * MNT: Re-rendered with conda-build 3.21.8, conda-smithy 3.19.0, and conda-forge-pinning 2022.05.09.07.08.30
    
    * Attempt to fix build
    * Explicitely set C and C++ compiler for tool build
    
    * Attempt to fix build
    * Cleanup

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,20 +1,30 @@
 @echo off
 
-echo v0.1.0-dev.4-0-g134e6f3-experimental/jiffies ^
-    > "%SRC_DIR%\MICROED-TOOLS-VERSION-FILE"
-del "%SRC_DIR%\VERSION"
+if not "%PKG_BUILDNUM%" == "0" set buildmetadata=%PKG_BUILDNUM%
+(
+    echo MICROED_TOOLS_VERSION_BRANCH=experimental/jiffies
+    echo MICROED_TOOLS_VERSION_BUILDMETA=%buildmetadata%
+    echo MICROED_TOOLS_VERSION_OBJECT_NAME=d0bd797
+    echo MICROED_TOOLS_VERSION_PRERELEASE=dev.5
+    echo MICROED_TOOLS_VERSION_STATE=
+    echo PACKAGE_VERSION_MAJOR=0
+    echo PACKAGE_VERSION_MINOR=1
+    echo PACKAGE_VERSION_PATCH=0
+    echo PACKAGE_VERSION_TWEAK=0
+) > "%SRC_DIR%\MICROED-TOOLS-VERSION-FILE"
 
 cmake -G "MinGW Makefiles" %CMAKE_ARGS%                               ^
+    -DBUILD_PYTHON_MODULE:BOOL=OFF                                    ^
+    -DCMAKE_C_COMPILER=gcc                                            ^
     -DCMAKE_C_FLAGS:STRING="%CFLAGS% -D_POSIX_C_SOURCE=200809L -Wall" ^
     -DCMAKE_COLOR_MAKEFILE:BOOL=OFF                                   ^
+    -DCMAKE_CXX_COMPILER=g++                                          ^
     -DCMAKE_CXX_FLAGS:STRING="%CXXFLAGS% -Wall"                       ^
+    -DNLOPT_LIBRARIES:PATH="%LIBRARY_LIB%\nlopt.lib"                  ^
     "%SRC_DIR%"
 if errorlevel 1 exit /b 1
 
 cmake --build . --parallel "%CPU_COUNT%"
-if errorlevel 1 exit /b 1
-
-cmake --build . --parallel "%CPU_COUNT%" --target man
 if errorlevel 1 exit /b 1
 
 cmake --install . --prefix "%PREFIX%"
@@ -23,4 +33,21 @@ if errorlevel 1 exit /b 1
 install -D -m 644                      ^
     "%SRC_DIR%\README"                 ^
     "%PREFIX%\share\%PKG_NAME%\README"
+if errorlevel 1 exit /b 1
+
+del CMakeCache.txt
+rd /q /s CMakeFiles
+cmake -G "NMake Makefiles"                    ^
+    -DBUILD_PYTHON_MODULE:BOOL=ON             ^
+    -DCMAKE_BUILD_TYPE:STRING=Release         ^
+    -DCMAKE_C_FLAGS:STRING="%CFLAGS% /W3"     ^
+    -DCMAKE_CXX_FLAGS:STRING="%CXXFLAGS% /W3" ^
+    -DPython3_EXECUTABLE:PATH="%PYTHON%"      ^
+    "%SRC_DIR%"
+if errorlevel 1 exit /b 1
+
+cmake --build . --config Release --parallel "%CPU_COUNT%" --target pysmv
+if errorlevel 1 exit /b 1
+
+cmake --install . --component module --config Release --prefix "%SP_DIR%"
 if errorlevel 1 exit /b 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,33 @@
 #! /bin/sh
 
-echo "v0.1.0-dev.4-0-g134e6f3-experimental/jiffies" \
-    > "${SRC_DIR}/MICROED-TOOLS-VERSION-FILE"
-rm "${SRC_DIR}/VERSION"
+iconv_args="-DIconv_INCLUDE_DIR:PATH=${CONDA_BUILD_SYSROOT}/usr/include"
+if test -n "${LD_RUN_PATH}"; then
+    iconv_args="${iconv_args} -DIconv_IS_BUILT_IN:BOOL=TRUE"
+elif test -n "${OSX_ARCH}"; then
+    iconv_args="${iconv_args} -DIconv_LIBRARY:PATH=${CONDA_BUILD_SYSROOT}/usr/lib/libiconv.dylib"
+fi
 
-cmake ${CMAKE_ARGS}                              \
+test "${PKG_BUILDNUM}" != "0" && buildmetadata="${PKG_BUILDNUM}"
+cat << EOF > "${SRC_DIR}/MICROED-TOOLS-VERSION-FILE"
+MICROED_TOOLS_VERSION_BRANCH=experimental/jiffies
+MICROED_TOOLS_VERSION_BUILDMETA=${buildmetadata}
+MICROED_TOOLS_VERSION_OBJECT_NAME=d0bd797
+MICROED_TOOLS_VERSION_PRERELEASE=dev.5
+MICROED_TOOLS_VERSION_STATE=
+PACKAGE_VERSION_MAJOR=0
+PACKAGE_VERSION_MINOR=1
+PACKAGE_VERSION_PATCH=0
+PACKAGE_VERSION_TWEAK=0
+EOF
+
+cmake ${CMAKE_ARGS} ${iconv_args}                \
+    -DBUILD_PYTHON_MODULE:BOOL=ON                \
     -DCMAKE_C_FLAGS:STRING="${CFLAGS} -Wall"     \
     -DCMAKE_CXX_FLAGS:STRING="${CXXFLAGS} -Wall" \
+    -DPython3_EXECUTABLE:PATH="${PYTHON}"        \
     "${SRC_DIR}"
 
 cmake --build . --parallel "${CPU_COUNT}"
-cmake --build . --parallel "${CPU_COUNT}" --target man
 cmake --install . --prefix "${PREFIX}"
 
 install -D -m 644                        \
